f5dc8901dac98e9aaaac68d89129c7dacdc7f2ee
mazo7474 <ismail.abdirahman@student.moringaschool.com>
2021-09-13 14:59:50 +0300

    add class in html

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
-
-
-
-
-
-
-
-
-
+    $("#agenda__image").click(function(){
+        $("#image__text").toggle();
+        $(".hide-image__text").toggle();
+      })
     
+      $("#development").click(function(){
+        $("#development-showing").toggle();
+        $(".hide-development-image").toggle();
+      })
+    
+      $("#productManagement").click(function(){
+        $("#product-management-showing").toggle();
+        $(".hide-product-management").toggle();
+      })
+    
+      //Insert white box upo hover
+      $(".Hover").hover(function(){
+        $(this).animate({opacity:'1'});
+      },
+      function(){
+        $(this).animate({opacity:'0'});
+      })              
+
+
+
+
+
+
+
+
+
 })
