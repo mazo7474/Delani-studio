a41b8019803dad22976823f8943b90fd0cee17b8
mazo7474 <ismail.abdirahman@student.moringaschool.com>
2021-09-13 15:05:16 +0300

    add js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,11 @@
 $(document).ready(function(){
     $("#agenda__image").click(function(){
-        $("#image__text").toggle();
-        $(".hide-image__text").toggle();
-      })
+        $("#design-showing").toggle();
+        $(".hide-design-showing").toggle();
+    })
     
-      $("#development").click(function(){
-        $("#development-showing").toggle();
-        $(".hide-development-image").toggle();
-      })
-    
-      $("#productManagement").click(function(){
-        $("#product-management-showing").toggle();
-        $(".hide-product-management").toggle();
-      })
-    
-      //Insert white box upo hover
-      $(".Hover").hover(function(){
-        $(this).animate({opacity:'1'});
-      },
-      function(){
-        $(this).animate({opacity:'0'});
-      })              
+      
+                  
 
 
 
