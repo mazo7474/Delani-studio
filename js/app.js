4c0a033bfd6643a6f1516f62ac2308eec463b793
mazo7474 <ismail.abdirahman@student.moringaschool.com>
2021-09-15 15:22:40 +0300

    redesign image

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
+// $(document).ready(function(){
+//     console.log("mazo")
+
+//     // jQuery methods go here...
+  
+//   });
+
+
 $(document).ready(function(){
+    //toggle between image and showing//
     $("#agenda__image").click(function(){
-        $("#design-showing").toggle();
-        $(".hide-design-showing").toggle();
+        $("#agenda__image").toggle();
+        $("#design-showing").show();
+        
     })
-    
-      
-                  
 
+    $("#development").click(function(){
+        $("#Dev_image").toggle();
+        $("#development-showing").show();
+    })
 
-
-
-
-
-
-
+    $("#product").click(function(){
+        $("#product_image").toggle();
+        $("#product-management-showing").show();
+    })
 
 })
